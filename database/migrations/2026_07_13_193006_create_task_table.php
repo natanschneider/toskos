4aeb0c118615879b9b9fe5c75eff52d43d7813c6
Natan Schneider <natan.schneider@deltasul.com.br>
2026-07-13 16:39:07 -0300

    *feat* create necessary tables

---
+++ database/migrations/2026_07_13_193006_create_task_table.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('status', function (Blueprint $table): void {
+            $table->id();
+            $table->string('name');
+        });
+
+        Schema::create('task', function (Blueprint $table): void {
+            $table->id();
+            $table->string('name', 255);
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
+            $table->timestamp('planned_start_date');
+            $table->timestamp('planned_end_date');
+            $table->string('responsible');
+            $table->string('supervisor');
+            $table->string('doc_file', 500)->nullable();
+            $table->unsignedBigInteger('status_id');
+            $table->string('created_by', 255);
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('status_id')->references('id')->on('status');
+            $table->foreign('project_id')->references('id')->on('project');
+            $table->foreign('parent_id')->references('id')->on('task');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task');
+        Schema::dropIfExists('status');
+    }
+};
