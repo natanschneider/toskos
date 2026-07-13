e4562207e5267e5c589ba4da1b59e98bdab3bbcb
Natan Schneider <natan.schneider@deltasul.com.br>
2026-07-13 12:10:16 -0300

    *feat* logotipos

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -4,12 +4,10 @@ export default function AppLogo() {
     return (
         <>
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                <AppLogoIcon className="size-6 fill-current text-white dark:text-black" />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
-                </span>
+            <div className="text-md ml-1 grid flex-1 text-left">
+                <span className="mb-0.5 truncate leading-tight font-semibold">Toskos</span>
             </div>
         </>
     );
